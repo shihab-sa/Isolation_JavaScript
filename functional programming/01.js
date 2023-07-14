e14bae2a534ca0771c917caf3a7742fa3e331195
game123ahmed@gmail.com <game123ahmed@gmail.com>
2023-07-14 22:32:25 +0600

    new logic added

---
+++ functional programming/01.js
@@ -0,0 +1,14 @@
+// এখানে দেখো গারভেজ একটা ডাটা কে pop() করে মুছে দিলাম!
+
+
+
+
+function garvesData(arr,...newData){
+    console.log(newData.pop());
+}
+
+let rewc = garvesData([
+    {name:'shihan',age:34},
+    {name:"rakib",age:44}      ],
+[{name:'sada',age:55}],(x)=>x.data === x.data)
+console.log(rewc);
